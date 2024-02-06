fcc21e92a9ef8092cef8f071196d29dfc984d6f1
DevHeun <dudwo5600@korea.ac.kr>
2024-02-06 14:44:49 +0900

    User data validation check

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,5 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto{
+    @IsString() // string이여야 하고
+    @MinLength(4) // 최소길이 4
+    @MaxLength(20) // 최대길이 20
     username: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/,{
+        message : 'only english and number are accpeted.'
+    })
     password: string;
 }
 
