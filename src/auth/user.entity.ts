d8ec473f5bc82b8477e5bb481d2b752ff5c27543
DevHeun <dudwo5600@korea.ac.kr>
2024-02-06 14:55:51 +0900

    unique username

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
 @Entity()
+@Unique(['username']) // unique한 값이어야 한다.
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
 import { Repository, DataSource } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
@@ -9,9 +9,18 @@ export class UserRepository extends Repository<User> {
     super(User, dataSource.createEntityManager());
   }
 
-  async createUser(authCredentialsDto : AuthCredentialsDto) : Promise<void>{
-    const {username, password} = authCredentialsDto;
-    const user = this.create({username,password});
-    await this.save(user);
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        //username 중복 에러 코드
+        throw new ConflictException('Existing username');
+      }else{
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
