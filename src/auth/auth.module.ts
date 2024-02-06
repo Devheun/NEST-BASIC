9ee8586f3385f7800c1c0589f21d5d9a0caffc8f
DevHeun <dudwo5600@korea.ac.kr>
2024-02-06 16:55:03 +0900

    create token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+import { PassportModule } from '@nestjs/passport';
+dotenv.config();
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({defaultStrategy : 'jwt'}),
+    JwtModule.register({
+      secret : process.env.SECRET_KEY,
+      signOptions : {
+        expiresIn : 3600
+      }
+    }),
+    TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
   providers: [AuthService, UserRepository]
 })
