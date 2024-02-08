0bc2235ddea8dae912b82900840208a1d0beb4f0
DevHeun <dudwo5600@korea.ac.kr>
2024-02-08 23:14:19 +0900

    add relation between user and board

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Board } from '../boards/board.entity';
 
 @Entity()
 @Unique(['username']) // unique한 값이어야 한다.
@@ -11,4 +12,10 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // type => Board는 Board 엔터티 타입이라고 정의
+  // board => board.user는 board 엔티티에서 user에 접근할 때
+  // eager true로 설정하면 user를 가져올 때 board도 같이 가져온다.
+  @OneToMany(type => Board, board => board.user, {eager : true}) 
+  boards: Board[];
 }
